5904d1682902059632aace0f1fad48d6805bfb2b
framzy <fardenramzy97@gmail.com>
2026-05-22 09:25:35 +0700

    add Tailwind CSS integration and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
-import "./styles/index.css";
+import Carts from "./pages/Carts";
+import "./index.css";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/carts" element={<Carts />} />
       </Routes>
     </Router>
   );
