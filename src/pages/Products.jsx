b7a2589f6d660b469bb84aea04d518aa15711316
framzy <fardenramzy97@gmail.com>
2026-06-03 00:03:35 +0700

    Add product images and refactor Products component for improved structure and styling

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,33 +1,26 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { useCart } from "../hooks/useCart";
-import PopupCart from "../components/PopupCart";
-import PopupCheckout from "../components/PopupCheckout";
-import ProductList from "../components/ProductList";
+import PopupCart from "../components/cart/PopupCart";
+import PopupCheckout from "../components/product/PopupCheckout";
+import ProductList from "../components/product/ProductList";
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
   const [showCheckout, setShowCheckout] = useState(false);
-  const { addToCart, clearCart, showPopup, setShowPopup } = useCart();
-
-  useEffect(() => {
-    fetch("/src/data/products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
+  const { clearCart, showPopup, setShowPopup } = useCart();
 
   const handleCheckout = () => {
     clearCart();
   };
 
   return (
-    <div className="min-h-screen bg-[var(--background)] overflow-hidden px-8 py-12 md:p-12">
+    <div className="min-h-screen bg-[var(--background-white)] overflow-hidden px-8 pt-20 pb-12 md:p-10 md:pt-24">
       <div className="w-full flex justify-center items-center pb-10">
         <motion.h1
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, ease: "easeInOut" }}
-          className="w-fit font-bold text-center text-3xl sm:text-4xl text-[var(--color-primary)] 
+          transition={{ duration: 0.5, ease: "easeInOut", delay: 0.4 }}
+          className="w-fit font-bold text-center text-2xl sm:text-4xl text-[var(--color-primary)] 
         px-15 py-2 shadow-[0_10px_8px_-10px_rgba(0,0,0,0.3)]"
         >
           Semua Produk
@@ -37,12 +30,11 @@ const Products = () => {
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, ease: "easeInOut" }}
+        transition={{ duration: 0.5, ease: "easeInOut", delay: 0.4 }}
         className="w-full flex flex-col justify-center items-center bg-[white] rounded-3xl shadow-lg"
-        id="section2"
       >
         <div className="w-full p-12">
-          <ProductList products={products} onAddToCart={addToCart} />
+          <ProductList productListOptions="all" />
         </div>
       </motion.div>
 
